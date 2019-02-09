b72bae4d08185ea2e1564844468c1cd322d5f495
utsab <utsabsaha@cm148-143.resnet.csumb.edu>
2019-02-08 20:10:28 -0800

    Finished tutorial

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './Home';
+import DynamicPage from './DynamicPage';
+import NoMatch from './NoMatch';
 
 const App = () => {
   return (
-    <div>something</div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/dynamic" component={DynamicPage} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
